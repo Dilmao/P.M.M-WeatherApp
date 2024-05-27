a82eb0c52b88296a9fa17a91ffa9670bf1a0c2e6
diego <200066@iessanalberto.com>
2024-05-27 11:01:30 +0200

    commit temperaturas en los 3 formatos

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.Text
 import androidx.lifecycle.lifecycleScope
 import com.example.weatherapp.data.RetrofitServiceFactory
 import com.example.weatherapp.data.model.RemoteResult
@@ -24,7 +25,11 @@ class MainActivity : ComponentActivity() {
         // CODIGO QUE NO FALLA:
         lifecycleScope.launch {
             clima = service.getWeather("93918e4c01b474b2757b449474dd8021", "jaca")
-            println("TEMPERATURA: ${clima!!.main.temp} grados kelvin")
+            println("TEMPERATURA: " +
+                    "${clima!!.main.temp}K - " +                        // Grados Kelvin
+                    "${clima!!.main.temp - 273.1}ºC - " +               // Grados Celsius
+                    "${(clima!!.main.temp - 273.1) * 9 / 5 + 32}ºF")    // Grados Fahrenheit
+            // 288.85K - 15.75ºC - 60.35ºF
         }
 
         super.onCreate(savedInstanceState)
